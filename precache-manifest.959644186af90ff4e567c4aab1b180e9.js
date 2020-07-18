694db8b67c6136c8e2761160af1e047222797aba
shivu <venkateshkonuku888@gmail.com>
2020-07-18 15:25:26 +0530

    Updates

---
+++ precache-manifest.959644186af90ff4e567c4aab1b180e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da889e91a58615ee4e63051e3a12c8e3",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "db6a559ddc4dd6111b39",
+    "url": "/news/static/css/main.b1bfbff1.chunk.css"
+  },
+  {
+    "revision": "cfda2f31ebfade2ee489",
+    "url": "/news/static/js/2.ac1686ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news/static/js/2.ac1686ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6a559ddc4dd6111b39",
+    "url": "/news/static/js/main.b5142888.chunk.js"
+  },
+  {
+    "revision": "f8fc2dd7a9d0a00b2624",
+    "url": "/news/static/js/runtime-main.e81905b5.js"
+  }
+]);
